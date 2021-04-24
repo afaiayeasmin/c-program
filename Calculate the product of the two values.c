2f3b0512fd45e526701867c6895db9b3149e6988
afaiayeasmin <82940963+afaiayeasmin@users.noreply.github.com>
2021-04-25 00:17:36 +0600

    To  Find the product
     Write a c program to calculate the product of two numbers.

---
+++ Calculate the product of the two values.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<math.h>
+
+int main()
+
+{
+
+    float a, b, result;
+
+    printf("\n Input the first value: ");
+
+    scanf("%f", &a);
+
+    printf("\n Input the second value: ");
+
+    scanf("%f", &b);
+
+    result = (a * b);
+
+    printf("\n Product of the above two values = %f\t", result);
+
+
+    return 0;
+}
