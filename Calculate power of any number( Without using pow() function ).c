0fcaf2fb99de817348f11695aa7fa4c57560dd71
afaiayeasmin <82940963+afaiayeasmin@users.noreply.github.com>
2021-05-06 00:50:53 +0600

    Write a C program to calculate power of any number without using pow( ) function
    calculate power of any number without using pow( ) function.

---
+++ Calculate power of any number( Without using pow() function ).c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int  main()
+{
+
+    int a,cube;
+
+    printf("Enter any integer number: ");
+
+    scanf("%d",&a);
+
+    //calculating cube
+    cube = (a*a*a);
+
+    printf("CUBE is: %d\n",cube);
+
+    return 0;
+
+}
