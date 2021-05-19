06701ec421a21d7f5177ed80ac7f9081c5207e39
afaiayeasmin <82940963+afaiayeasmin@users.noreply.github.com>
2021-05-20 00:37:53 +0600

    Multiplication of two integer number using plus operator.
     Write a C program to calculate Multiplication of two integer number using plus operator.

---
+++ Multiplication of two numbers using plus (+) operator.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b;
+    int mul,loop;
+
+    printf("Enter first number: ");
+
+    scanf("%d",&a);
+
+    printf("Enter second number: ");
+
+    scanf("%d",&b);
+
+    mul=0;
+
+    for(loop=1; loop<=b; loop++)
+    {
+
+        mul += a;
+    }
+
+    printf("Multiplication of %d and %d is: %d\n",a,b,mul);
+
+    return 0;
+}
