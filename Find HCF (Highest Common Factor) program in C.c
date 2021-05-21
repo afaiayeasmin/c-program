623b092e2824b52b14eef59a436ed7d9aa198cd6
afaiayeasmin <82940963+afaiayeasmin@users.noreply.github.com>
2021-05-22 00:09:39 +0600

    Write a C program to Find HCF(Highest Common Factors) using Function.
    Find HCF(Highest Common Factors).

---
+++ Find HCF (Highest Common Factor) program in C.c
@@ -0,0 +1,36 @@
+int findHcf(int a,int b)
+{
+    int temp;
+
+    if(a==0 || b==0)
+    return 0;
+    while(b!=0)
+    {
+        temp = a%b;
+        a    = b;
+        b    = temp;
+    }
+    return a;
+}
+int main()
+{
+    int a,b;
+    int hcf;
+
+    printf("Enter first number: ");
+
+    scanf("%d",&a);
+
+    printf("Enter second number: ");
+
+    scanf("%d",&b);
+
+    hcf=findHcf(a,b);
+
+    printf("HCF (Highest Common Factor) of %d,%d is: %d\n",a,b,hcf);
+
+
+    return 0;
+
+
+}
